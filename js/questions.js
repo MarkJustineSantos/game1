58d149078ab05fefb7e63d5729d8123ba55895d1
Mark Justine Santos <114572934+MarkJustineSantos@users.noreply.github.com>
2024-06-15 09:21:35 +0800

    FIRST-COMMIT

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What was Rizal's first poem?",
+    answer: "Sa aking mga kababata",
+    options: [
+      "Mi último adiós",
+      "Sa aking mga kababata",
+      "Himno al Trabajo",
+      "A la juventud filipina"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the title of Rizal's unfinished novel in Tagalog?",
+    answer: "Nakamisa",
+    options: [
+      "Laban Bayan",
+      "Lapastangan",
+      "Nakamisa",
+      "Huwaran"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who was the oldest sister of Rizal?",
+    answer: "Saturnina",
+    options: [
+      "Paciano Rizal",
+      "Narcisa Mercado",
+      "Saturnina",
+      "Josefa Mercado"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When and where did Rizal start formal schooling?",
+    answer: "Biñan on 1869",
+    options: [
+      "Biñan on 1869",
+      "Ateneo de Manila on 1872",
+      "University of Santo Tomas on 1877",
+      "City College of Calamba on 1878"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What was Rizal’s first winning literary piece on the competition sponsored by the Artistic-Literary Lyceum?",
+    answer: "A la juventud filipina",
+    options: [
+      "Mi último adiós",
+      "Sa aking mga kababata",
+      "Himno al Trabajo",
+      "A la juventud filipina"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What is the complete legal name of Dr. Jose Rizal?",
+    answer: "José Protacio Rizal Mercado y Alonso Realonda",
+    options: [
+      "José Protacio Rizal Mercado y Alonso Realonda",
+      "Dr. Josito Alonso Realonda Mercado Rizal",
+      "Dr. Jose Protacio Alonso Realondo Martinez Rizal",
+      "Dr. Jose Pedro Alonso Realonda Mercado Rizal"
+    ]
+   },
+   {
+    numb: 7,
+    question: "What was the date of birth of Dr. Jose Rizal?",
+    answer: "June 19, 1861",
+    options: [
+      "June 19, 1861",
+      "May 19, 1761",
+      "July 19, 1871",
+      "June 18, 1961"
+    ]
+   },
+   {
+    numb: 8,
+    question: "What is the Spanish name for a Filipino native, which the Filipinos resented?",
+    answer: "Indio",
+    options: [
+      "Indio",
+      "Hudio",
+      "Pinoy",
+      "Alferez"
+    ]
+   },
+   {
+    numb: 9,
+    question: "Name Rizal's host at Wilhelmsfeld.",
+    answer: "Karl Ulmer",
+    options: [
+     "Karl Ulmer",
+     "Dr. Reinhold Rost",
+      "Dr. Ferdinand Blumettritt",
+     "Dr. Jagor"
+    ]
+   },
+   {
+    numb: 10,
+    question: "At 16, Rizal experienced his first romance. He fell in love with which girl?",
+    answer: "Segunda Katigbak",
+    options: [
+     "Leonor Gonzalez",
+     "Leonor Rivera",
+      "Segunda Katigbak",
+     "Getrude Beckett"
+    ]
+   },
+];
